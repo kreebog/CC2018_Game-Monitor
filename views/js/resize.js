57ea50358f34787b51b3108be97503ab91c518ee
Russell Estes <estex198@gmail.com>
2018-07-11 00:09:18 -0400

    sorta kinda fixed the scale-up issue... for real this time...

--- views/js/resize.js
+++ views/js/resize.js
@@ -10,14 +10,22 @@ window.onload = function () {
         var cell = document.querySelector(".maze_cell");
         var cell_width = parseInt(window.getComputedStyle(cell, null).getPropertyValue("width"), 10);
         
-        maze_width = cols * cell_width;
+		maze_width = cols * cell_width;
+		
         resizeContainer(maze_width);
     });
 
     function resizeContainer(maze_width) {
     	// Width of parent container of maze_container
 		let parent_width = parseInt(getComputedStyle(document.getElementById("main"), null).getPropertyValue("width"), 10);
-		let zoom_value = ((parent_width / maze_width)*90).toFixed(6).toString() + "%";
+		 
+		 let zoom_value;
+		 if (maze_width > parent_width) {
+			zoom_value = ((parent_width / maze_width)*90).toFixed(6).toString() + "%";
+		 }
+		 else {
+			 zoom_value = "100%";
+		 }
 		document.getElementById('maze_container').style.zoom = zoom_value;
 	
 	
@@ -25,3 +33,5 @@ window.onload = function () {
 	
 };
 //# sourceMappingURL=resize.js.map
+
+
