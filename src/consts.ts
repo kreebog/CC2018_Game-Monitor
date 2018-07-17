b94b58870bd00033ac6c0a533f0e94a0644063d3
James Dean <kreebog@gmail.com>
2018-07-17 09:54:36 -0400

    updated consts default values

--- src/consts.ts
+++ src/consts.ts
@@ -7,23 +7,23 @@ import path from 'path';
 export const NODE_ENV = process.env.NODE_ENV || 'production';
 
 // load env vars (or defaults if not found - aka PROD)
-const MAZE_SVC_HOST = process.env.MAZE_SVC_URL || 'http://maze.code-camp-2018.svc';
-const MAZE_SVC_PORT = process.env.MAZE_SVC_PORT || 80;
+const MAZE_SVC_HOST = process.env.MAZE_SVC_URL || 'http://maze-service.code-camp.svc';
+const MAZE_SVC_PORT = process.env.MAZE_SVC_PORT || 8080;
 
-const SCORE_SVC_HOST = process.env.SCORE_SVC_URL || 'http://score.code-camp-2018.svc';
-const SCORE_SVC_PORT = process.env.SCORE_SVC_PORT || 80;
+const SCORE_SVC_HOST = process.env.SCORE_SVC_URL || 'http://score-service.code-camp.svc';
+const SCORE_SVC_PORT = process.env.SCORE_SVC_PORT || 8080;
 
-const TEAM_SVC_HOST = process.env.TEAM_SVC_URL || 'http://team.code-camp-2018.svc';
-const TEAM_SVC_PORT = process.env.TEAM_SVC_PORT || 80;
+const TEAM_SVC_HOST = process.env.TEAM_SVC_URL || 'http://team-service.code-camp.svc';
+const TEAM_SVC_PORT = process.env.TEAM_SVC_PORT || 8080;
 
-const GAME_SVC_HOST = process.env.GAME_SVC_URL || 'http://game-server.code-camp-2018.svc';
-const GAME_SVC_PORT = process.env.GAME_SVC_PORT || 80;
+const GAME_SVC_HOST = process.env.GAME_SVC_URL || 'http://game-server.code-camp.svc';
+const GAME_SVC_PORT = process.env.GAME_SVC_PORT || 8080;
 
-const GAME_SVC_HOST_EXT = process.env.GAME_SVC_EXT_URL || 'http://code-camp-2018.com';
+const GAME_SVC_HOST_EXT = process.env.GAME_SVC_EXT_URL || 'http://game.code-camp-2018.com';
 const GAME_SVC_PORT_EXT = process.env.GAME_SVC_EXT_PORT || 80;
 
-export const GAME_MON_HOST = process.env.GAME_MON_URL || 'http://monitor.code-camp.svc';
-export const GAME_MON_PORT = process.env.GAME_MON_PORT || 80;
+export const GAME_MON_HOST = process.env.GAME_MON_URL || 'http://game-monitor.code-camp.svc';
+export const GAME_MON_PORT = process.env.GAME_MON_PORT || 8080;
 
 // construct base URLs
 export const MAZE_SVC_URL = format('%s:%s', MAZE_SVC_HOST, MAZE_SVC_PORT);
